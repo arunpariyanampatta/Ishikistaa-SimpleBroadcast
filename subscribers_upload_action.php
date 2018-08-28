e512fc34de5e5ec4cd684ac8682fe9175238451f
DELL <DELL@DELL-PC>
2018-08-28 11:07:23 +0300

    commented extra consumers

--- subscribers_upload_action.php
+++ subscribers_upload_action.php
@@ -11,11 +11,11 @@ if(isset($_FILES['file']['name'])) {
         $file = $path;
         $con = connect_db();
         $table = "tbl_trivia_promotion_".date("Ymd");
+        $truncate = "TRUNCATE ".$table;
+        mysqli_query($conn,$truncate);
         $query = "LOAD DATA LOCAL INFILE '".$file."' INTO TABLE `".$table."`  FIELDS TERMINATED BY ','  LINES TERMINATED BY '".'\n'. "' (MSISDN) SET ID = NULL";
         $result = mysqli_query($con, $query);
-        
         $n = $result->affected_rows;
-        
         mysqli_close($con);
         unlink($path);
         header("Location: subscribers_upload.php?uploaded=1&msisdn_nm=".$n);
